38afaa3d6cb6ad84b9c8a1ab1cd9aa7a06baebaa
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-03-14 11:09:24 -0500

    Coursework

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,9 +1,12 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -14,12 +17,22 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
 
-        if (validAttempt) {
-            response.sendRedirect("/profile");
-        } else {
-            response.sendRedirect("/login");
+        if(username.equals("username") && password.equals("password")){
+            Cookie loginCookie = new Cookie("user", username);
+            //setting cookie to expire in 5 mins
+            loginCookie.setMaxAge(5*60);
+            response.addCookie(loginCookie);
+            response.sendRedirect("LoginSuccess.jsp");
+        }else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            try {
+                rd.include(request, response);
+            } catch (ServletException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
